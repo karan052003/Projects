9a78d18890cda343e36089f253a0a2e60b4956d8
Karan Sarode <karansarode218@gmail.com>
2023-06-09 13:34:43 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+package QuizGame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Quiz quiz = new Quiz();
+    }
+}
+
+package runtime;
+
+public class Quiz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
